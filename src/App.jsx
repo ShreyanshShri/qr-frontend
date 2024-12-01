07dbf165b8bbb9abeea070c448112a76fac5e1e6
Shreyansh Shrivastva <shreyanshshrivastva@gmail.com>
2024-12-01 13:25:22 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.css'
+
+import Homepage from "./pages/Homepage"
+import Loginpage  from './pages/Loginpage'
+import Studentdashboard from './pages/Studentdashboard'
+import Teacherdashboard from './pages/Teacherdashboard'
+
+function App() {
+
+  return (
+    <>
+    {/* <Homepage /> */}
+      <Router>
+        {/* <Navbar switchNav={switchNav} isSmall={isSmall} /> */}
+        <Routes>
+          <Route exact path='/' element={<Homepage />} />
+          <Route exact path='/login' element={<Loginpage />} />
+          <Route exact path='/student-dashboard' element={<Studentdashboard />} />
+          <Route exact path='/teacher-dashboard' element={<Teacherdashboard />} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
